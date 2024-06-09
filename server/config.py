b50c142905e84447be81ea2daa48c4fe5b4c0e07
Priscilla-22 <priscillamw123@gmail.com>
2024-06-09 22:21:00 +0300

    feat: use environment variables for MPESA consumer key, secret, and callback URL

--- server/config.py
+++ server/config.py
@@ -7,12 +7,8 @@ class Config:
         os.environ.get("DATABASE_URI") or "sqlite:///Bahari-Bites.sqlite"
     )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    MPESA_CONSUMER_KEY = os.environ.get(
-        "UGUC1ZsTG7mwIC44IbCw95uBU6R8C4kSdb94qjGXTcWNSqim"
-    )
-    MPESA_CONSUMER_SECRET = os.environ.get(
-        "oa7SwdPfrky8AFVSxXgzj2cjqbMGSmqB1hNRtAes8buVF36GT1b69UGYnAhgMaU3"
-    )
+    MPESA_CONSUMER_KEY = os.environ.get("MPESA_CONSUMER_KEY")
+    MPESA_CONSUMER_SECRET = os.environ.get("MPESA_CONSUMER_SECRET")
     MPESA_SHORTCODE = os.environ.get("MPESA_SHORTCODE")
     MPESA_PASSKEY = os.environ.get("MPESA_PASSKEY")
-    MPESA_CALLBACK_URL = os.environ.get("https://bahari-bites-backend.onrender.com")
+    MPESA_CALLBACK_URL = os.environ.get("MPESA_CALLBACK_URL")
