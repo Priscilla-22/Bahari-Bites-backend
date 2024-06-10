ab03e9dba7a6b825e6e00b7ea44cbd25527fd11b
Priscilla-22 <priscillamw123@gmail.com>
2024-06-10 20:58:22 +0300

    "Deleted initial migration file and removed database schema"

--- migrations/versions/f035a3fcad7d_initial_migration.py
+++ migrations/versions/f035a3fcad7d_initial_migration.py
@@ -1,8 +1,8 @@
 """Initial migration
 
-Revision ID: ff26cacdec37
+Revision ID: f035a3fcad7d
 Revises: 
-Create Date: 2024-06-10 12:05:56.742988
+Create Date: 2024-06-10 20:55:05.044696
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = 'ff26cacdec37'
+revision = 'f035a3fcad7d'
 down_revision = None
 branch_labels = None
 depends_on = None
