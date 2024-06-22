eba6744d37edd6219584e6c7e8f23cfd6139158f
Priscilla-22 <priscillamw123@gmail.com>
2024-06-22 13:50:28 +0300

    "Updated simulate_mpesa_callback call to pass payment_response argument"

--- server/resources.py
+++ server/resources.py
@@ -640,7 +640,7 @@ class ReservationResource(Resource):
         )
 
         if args["simulate"]:
-            simulate_mpesa_callback()
+            simulate_mpesa_callback(payment_response)
 
         if payment_response.get("ResponseCode") == "0":
             mpesa_transaction = MpesaTransaction(
